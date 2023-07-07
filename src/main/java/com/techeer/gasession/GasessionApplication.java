09fe6b86d6bcb0d00a0bdc6e9d979c0edc7ada0f
jiminsong490 <misosjm@naver.com>
2023-07-08 04:44:30 +0900

    chore : init

---
+++ src/main/java/com/techeer/gasession/GasessionApplication.java
@@ -0,0 +1,13 @@
+package com.techeer.gasession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasessionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techeer/gasession/GasessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techeer.gasession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
